b500b1456536bf15030ee52659e115ee25bc6624
MarcHartleib <marc.hartleib91@gmail.com>
2023-02-21 11:16:58 +0100

    added motorcycleControllerTest class

---
+++ motorcycle/src/test/java/com/petproject/motorcycle/controller/MotorcycleControllerTest.java
@@ -0,0 +1,24 @@
+package com.petproject.motorcycle.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MotorcycleControllerTest {
+
+    @Test
+    void getMotorcycle() {
+    }
+
+    @Test
+    void postMotorcycle() {
+    }
+
+    @Test
+    void putMotorcycle() {
+    }
+
+    @Test
+    void deleteMotorcycle() {
+    }
+}
