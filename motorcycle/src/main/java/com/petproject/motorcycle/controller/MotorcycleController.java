20a0b0eaebfefdc702684a20eddc01703038d3ae
MarcHartleib <marc.hartleib91@gmail.com>
2023-02-21 11:00:09 +0100

    get, post, put and delete mappings added

--- motorcycle/src/main/java/com/petproject/motorcycle/controller/MotorcycleController.java
+++ motorcycle/src/main/java/com/petproject/motorcycle/controller/MotorcycleController.java
@@ -1,10 +1,31 @@
 package com.petproject.motorcycle.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/motorcycle")
 public class MotorcycleController {
-    
+
+    int testNumber = 1;
+
+
+    @GetMapping
+    public String getMotorcycle() {
+        return "Get";
+    }
+
+    @PostMapping
+    public void postMotorcycle(@RequestBody String text) {
+        System.out.println(text);;
+    }
+
+    @PutMapping
+    public void putMotorcycle(@RequestBody int number) {
+        System.out.println(testNumber + number);
+    }
+
+    @DeleteMapping
+    public String deleteMotorcycle() {
+        return "Delete";
+    }
 }
