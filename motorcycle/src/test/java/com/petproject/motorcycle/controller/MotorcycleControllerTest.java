eaaf01e214b35aec4a9ccd60984259841b977f5b
MarcHartleib <marc.hartleib91@gmail.com>
2023-02-21 11:28:54 +0100

    getMotorcycle test

--- motorcycle/src/test/java/com/petproject/motorcycle/controller/MotorcycleControllerTest.java
+++ motorcycle/src/test/java/com/petproject/motorcycle/controller/MotorcycleControllerTest.java
@@ -1,13 +1,24 @@
 package com.petproject.motorcycle.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@WebMvcTest(MotorcycleController.class)
 class MotorcycleControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void getMotorcycle() {
+    void getMotorcycle() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/motorcycle")
+                        .content(String.valueOf(MediaType.APPLICATION_JSON)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
