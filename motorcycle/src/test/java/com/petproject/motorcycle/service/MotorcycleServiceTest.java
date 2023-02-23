656f01b9242e7248db7739c96bbea5ba37323800
MarcHartleib <marc.hartleib91@gmail.com>
2023-02-23 12:54:28 +0100

    added MotorcycleServiceTests

---
+++ motorcycle/src/test/java/com/petproject/motorcycle/service/MotorcycleServiceTest.java
@@ -0,0 +1,62 @@
+package com.petproject.motorcycle.service;
+
+import com.petproject.motorcycle.data.Manufacturer;
+import com.petproject.motorcycle.data.ModelType;
+import com.petproject.motorcycle.data.Motorcycle;
+import com.petproject.motorcycle.motorcycleRepository.MotorcycleRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class MotorcycleServiceTest {
+
+    private final Motorcycle motorcycle = new Motorcycle(Manufacturer.HONDA,
+            ModelType.ADVENTURE,
+            "CRF1100L Africa Twin - Adventure Sports",
+            2023,
+            "2-Stroke",
+            1084,
+            "liquid cooled",
+            100,
+            "chain",
+            18.8,
+            false);
+    MotorcycleRepository motorcycleRepository = mock(MotorcycleRepository.class);
+    MotorcycleService motorcycleService = new MotorcycleService(motorcycleRepository);
+
+    @Test
+    void getAllMotorcycles() {
+        motorcycleService.getAllMotorcycles();
+
+        verify(motorcycleRepository).findAll();
+    }
+
+    @Test
+    void save() {
+        motorcycleService.save(motorcycle);
+
+        verify(motorcycleRepository).save(motorcycle);
+    }
+
+    @Test
+    void update() {
+        motorcycleService.update(motorcycle);
+
+        verify(motorcycleRepository).save(motorcycle);
+    }
+
+    @Test
+    void delete() {
+        motorcycleService.delete(motorcycle);
+
+        verify(motorcycleRepository).delete(motorcycle);
+    }
+
+    @Test
+    void getMotorcycleById() {
+        motorcycleService.getMotorcycleById(1);
+
+        verify(motorcycleRepository).findById(1L);
+    }
+}
