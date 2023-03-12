93155108b0e71443aeadb90ab18c61b196616e8e
MarcHartleib <marc.hartleib91@gmail.com>
2023-03-12 14:36:37 +0100

    delete works

--- frontend/motorcycle/src/components/pages/MotorcyclesPage.jsx
+++ frontend/motorcycle/src/components/pages/MotorcyclesPage.jsx
@@ -1,33 +1,45 @@
 import LinkButton from "../atoms/LinkButton";
 import NavBar from "../organisms/NavBar";
-import Motorcycle from '../atoms/Motorcycle';
+import Motorcycle from "../atoms/Motorcycle";
+import DeleteButton from "../atoms/DeleteButton";
 
-export default function MotorcyclesPage ( data ) {
-    
+export default function MotorcyclesPage(data) {
+  function handleDelete(mocy) {
+    fetch("/motorcycle", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(mocy),
+    }).then((res) =>
+      res.status >= 400 ? alert("Error") : (window.location = "/motorcycles")
+    );
+  }
 
-    if (!data) {
-        return (
-        <div>
-            <NavBar>
-                <LinkButton name={'Home'} pageLink={'/'}/>
-                <LinkButton name={'Logout'}/>
-            </NavBar>
-            <p>No Data</p>
-        </div>
-
-        )
-    } else {
-
+  if (!data) {
     return (
-        <div>
-            <NavBar>
-                <LinkButton name={'Home'} pageLink={'/'}/>
-                <LinkButton name={'Logout'}/>
-            </NavBar>
-            {data.map((mocy) => (
-                <Motorcycle key={mocy.id} mocy={mocy}/>
-            ))}
-        </div>
-    )
-        }
+      <div>
+        <NavBar>
+          <LinkButton name={"Home"} pageLink={"/"} />
+          <LinkButton name={"Logout"} />
+        </NavBar>
+        <p>No Data</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <NavBar>
+          <LinkButton name={"Home"} pageLink={"/"} />
+          <LinkButton name={"Logout"} />
+        </NavBar>
+        {data.map((mocy) => (
+          <div key={mocy.id}>
+            <Motorcycle mocy={mocy} />
+            <DeleteButton onClick={() => handleDelete(mocy)} />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
